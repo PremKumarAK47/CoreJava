338c234a979d22fea38ff170af1dd5d65005351a
PremKumarAK47 <premkumar147369@gmail.com>
2023-01-30 02:24:16 +0530

    Genric

--- MapInter/src/genericsPractice/GenericMethod.java
+++ MapInter/src/genericsPractice/GenericMethod.java
@@ -1,18 +1,24 @@
 package genericsPractice;
 
-public class GenericMethod {
+public class GenericMethod<K> {
 	
    public static <T> void fun1(T t) 
     {	
 		System.out.println(t);
 	}
-
+public static <K,V> void fun2(K k,V v)
+{
+	System.out.println(k+" ============ "+v);
+}
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 		fun1(0);
+		fun2(10,0);
 		fun1("Hello");
+		fun2(20,"Hello");
 		
+		fun2(30,new Student(10, "n1", 500));
 		fun1(new Student(10, "n1", 500));
 
 	}
