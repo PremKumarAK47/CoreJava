8c0830b71419bf394f0dc3df00cb1bf6153b715b
PremKumarAK47 <premkumar147369@gmail.com>
2023-03-04 20:16:21 +0530

    BankDescription

--- WorkingWithAbstractClass/src/bankSystem/Demo.java
+++ WorkingWithAbstractClass/src/bankSystem/Demo.java
@@ -4,6 +4,13 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Bank b1= new SBI();
+		Bank b2=new PNB();
+		Bank b3= new ICIC();
+		System.out.println("Rate of Interest of SBI is: "+b1.getRateOfInterest()+" %");
+		System.out.println("Rate of Interest of PNB is: "+b2.getRateOfInterest()+" %");
+		System.out.println("Rate of Interest of ICIC is: "+b3.getRateOfInterest()+" %");
 
 	}
 
