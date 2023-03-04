0b1f1534ad4a393e384551b64eb44cbf768db150
PremKumarAK47 <premkumar147369@gmail.com>
2023-03-04 19:37:42 +0530

    AbstractConcept

--- WorkingWithAbstractClass/src/consForAbstract/package-info.java
+++ WorkingWithAbstractClass/src/consForAbstract/package-info.java
@@ -1,1 +1,18 @@
 package consForAbstract;
+
+/*
+we make Abstract class to hide the implementation the procedure
+and showing the functionality to user, abstract class have everything 
+which have the a normal class,like constructor implementation and much more thing 
+
+Then Question arises why do we make abstract class
+Suppose there are many classes which has some common functionality then if we
+want with them all, so we have to solution either 
+1-we write every functionality in that classes every time.
+2- we can write once and use no. of times we want
+so make a super class of all  of them as abstract class and extend them in all
+if some property is same but functionality is different then make that method 
+abstract and inherit them in all and define accordingly. 
+
+
+*/
