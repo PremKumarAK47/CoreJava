ec0fb20a75ae9d7e0688c50b0f4ef051acd6d75b
PremKumarAK47 <premkumar147369@gmail.com>
2023-03-05 16:16:10 +0530

    Abstract

--- WorkingWithAbstractClass/src/bankSystem/Demo.java
+++ WorkingWithAbstractClass/src/bankSystem/Demo.java
@@ -3,7 +3,7 @@ package bankSystem;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		Bank b1= new SBI();
 		Bank b2=new PNB();
@@ -11,7 +11,18 @@ public class Demo {
 		System.out.println("Rate of Interest of SBI is: "+b1.getRateOfInterest()+" %");
 		System.out.println("Rate of Interest of PNB is: "+b2.getRateOfInterest()+" %");
 		System.out.println("Rate of Interest of ICICI is: "+b3.getRateOfInterest()+" %");
-
+     
+		/*
+		 * Here if you notice that there is a relationship stabilized is called
+		 * Parent class reference and child class object
+		 * By this we can call all the  method of parent class or the method which 
+		 * are overridden in the child class.
+		 * If we want to call the child's specific method then we need to downcast the
+		 * child class object and hold into the child class variable
+		 * then we can call,
+		 */
+		
+		
 	}
 
 }
