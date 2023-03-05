b5233cac21f7b0e8637c45d66a1d8805ba350e5c
PremKumarAK47 <premkumar147369@gmail.com>
2023-03-05 16:46:47 +0530

    Inteface

--- WorkingWithInterface/src/module-info.java
+++ WorkingWithInterface/src/module-info.java
@@ -1,2 +1,14 @@
 module WorkingWithInterface {
+	
+	/*
+	 * Here we will work with InterFace
+	 * InerFace is used to achieved to get 100% ABSTRACATION
+	 * INTErface is blueprint of class
+	 * just like class is blue print of objects
+	 * interface can be used as method parameter and return type as well
+	 * There are only two value possible for Interface
+	 * Either it's child(Implementing Class) or null values;
+	 * here we will take a example in which we will use this
+	 * as a method return type of Interface
+	 */
 }
