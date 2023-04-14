18f39e065b927a70813cdacfe6abed6f72f0a59a
PremKumarAK47 <premkumar147369@gmail.com>
2023-04-14 16:15:10 +0530

    MethodReference

--- WorkingWithFunctionalnterface/src/functionMethodWithArgument/Demo.java
+++ WorkingWithFunctionalnterface/src/functionMethodWithArgument/Demo.java
@@ -1,9 +1,34 @@
 package functionMethodWithArgument;
 
 public class Demo {
+	
+	public static void funX(String n)
+	{
+		System.out.println("Welcome to LE By Static  "+n);
+	}
+	
+	public void funY(String n)
+	{
+		System.out.println("By NonStatic "+n);
+	}
+	
+	public Demo() {
+		// TODO Auto-generated constructor stub
+	}
+	public Demo(String n) {
+		// TODO Auto-generated constructor stub
+		System.out.println("By Constructor "+n);
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
+		InterParameter i1= Demo::funX;
+		InterParameter i2= new Demo()::funY;
+		InterParameter i3= Demo::new;
+		i1.sayHello("Ranjeet");
+		i2.sayHello("Ranjeet");
+		i3.sayHello("Ranjeet");
 
 	}
 
